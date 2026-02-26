4b621aa458d53d010d94670804e395dedae3d602
Levin Bänninger <levin@baenninger.me>
2026-02-26 21:04:16 +0100

    feat: set up convex and clerk

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
 import { Button } from '@/components/ui/button';
 import { Text } from '@/components/ui/text';
+import { api } from '@/convex/_generated/api';
+import { useQuery } from 'convex/react';
 import { Stack } from 'expo-router';
 import { View } from 'react-native';
 
 const Index = () => {
+  const quests = useQuery(api.quests.list);
+
   return (
     <>
       <Stack.Screen options={{ headerTitle: 'Home' }} />
@@ -11,6 +15,7 @@ const Index = () => {
         <Button onPress={() => alert('Hello User!')} variant='outline'>
           <Text>Click me</Text>
         </Button>
+        <Text>{quests?.map((quest) => quest.title).join(', ')}</Text>
       </View>
     </>
   );
---
+++ components/ui/social-connections.tsx
@@ -0,0 +1,56 @@
+import { Button } from '@/components/ui/button';
+import { Text } from '@/components/ui/text';
+import { useSSO } from '@clerk/clerk-expo';
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+import { useEffect } from 'react';
+import { Image, Platform, View } from 'react-native';
+
+WebBrowser.maybeCompleteAuthSession();
+
+function useWarmUpBrowser() {
+  useEffect(() => {
+    if (Platform.OS === 'web') return;
+    void WebBrowser.warmUpAsync();
+    return () => {
+      void WebBrowser.coolDownAsync();
+    };
+  }, []);
+}
+
+export function SocialConnections() {
+  useWarmUpBrowser();
+  const { startSSOFlow } = useSSO();
+
+  async function onGooglePress() {
+    try {
+      const { createdSessionId, setActive } = await startSSOFlow({
+        strategy: 'oauth_google',
+        redirectUrl: AuthSession.makeRedirectUri(),
+      });
+
+      if (createdSessionId && setActive) {
+        await setActive({ session: createdSessionId });
+      }
+    } catch (err) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }
+
+  return (
+    <View>
+      <Button
+        variant='outline'
+        size='sm'
+        className='w-full'
+        onPress={onGooglePress}
+      >
+        <Image
+          className='size-4'
+          source={{ uri: 'https://img.clerk.com/static/google.png?width=160' }}
+        />
+        <Text>Google</Text>
+      </Button>
+    </View>
+  );
+}
