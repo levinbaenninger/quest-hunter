f5831eb807e81bd44d90c46ceb434192d298e854
Levin Bänninger <levin@baenninger.me>
2026-02-28 15:03:02 +0100

    refactor: simplify QuestsScreen component by removing unused query and rendering logic

--- app/(tabs)/(quests)/index.tsx
+++ app/(tabs)/(quests)/index.tsx
@@ -1,21 +1,7 @@
 import Screen from "@/components/ui/screen";
-import { Text } from "@/components/ui/text";
-import { api } from "@/convex/_generated/api";
-import { useQuery } from "convex/react";
-import { View } from "react-native";
 
 const QuestsScreen = () => {
-  const quests = useQuery(api.quests.list);
-
-  return (
-    <Screen>
-      <View className="gap-3">
-        {quests?.map((quest) => (
-          <Text key={quest._id}>{quest.name}</Text>
-        ))}
-      </View>
-    </Screen>
-  );
+  return <Screen />;
 };
 
 export default QuestsScreen;
