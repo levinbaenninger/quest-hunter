1d6753dc4ed25ba5bf33e04c6d36bb7fea5f042f
Levin Bänninger <levin@baenninger.me>
2026-03-02 11:15:34 +0100

    refactor: remove dark theme support and clean up theme configuration

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,20 @@
 import { convex } from "@/lib/convex-client";
 import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { PortalHost } from "@rn-primitives/portal";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { useColorScheme } from "react-native";
 import "../global.css";
 
 const RootLayoutNav = () => {
   const { isSignedIn, isLoaded } = useAuth();
-  const colorScheme = useColorScheme();
 
   if (!isLoaded) return null;
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={DefaultTheme}>
       <Stack>
         <Stack.Protected guard={isSignedIn ?? false}>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
@@ -45,7 +39,7 @@ const RootLayout = () => {
       tokenCache={tokenCache}
     >
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-        <StatusBar style="auto" />
+        <StatusBar style="dark" />
         <RootLayoutNav />
         <PortalHost />
       </ConvexProviderWithClerk>
